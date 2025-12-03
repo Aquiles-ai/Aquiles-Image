2ea2d9a18cd7073ff5d9d2821f5f891951f9addd
F4k3r22 <fredyriveraacevedo13@gmail.com>
2025-12-02 18:42:33 -0600

    Minor update to PipelineZImage for optimizations; it needs to be validated.

--- aquilesimage/pipelines/pipelines.py
+++ aquilesimage/pipelines/pipelines.py
@@ -7,17 +7,19 @@ except ImportError as e:
     pass
 try:
     from diffusers.pipelines.z_image.pipeline_z_image import ZImagePipeline
+    from diffusers.models.transformers.transformer_z_image import ZImageTransformer2DModel
 except ImportError as e:
     print("Error import ZImagePipeline")
     pass
 from diffusers.models.auto_model import AutoModel
-from transformers import Mistral3ForConditionalGeneration
+from transformers import Mistral3ForConditionalGeneration, AutoModelForCausalLM, AutoTokenizer
 from diffusers.pipelines.flux.pipeline_flux_kontext import FluxKontextPipeline
 import torch
 import os
 import logging
 from aquilesimage.models import ImageModel
 from aquilesimage.utils import setup_colored_logger
+from diffusers import AutoencoderKL, FlowMatchEulerDiscreteScheduler
 
 
 logger_p = setup_colored_logger("Aquiles-Image-Pipelines", logging.DEBUG)
@@ -249,26 +251,39 @@ class PipelineZImage:
         self.model_path = model_path or os.getenv("MODEL_PATH")
         try:
             self.pipeline: ZImagePipeline | None = None
+            self.transformer_z: ZImageTransformer2DModel | None = None
         except Exception as e:
             self.pipeline = None
+            self.transformer_z = None
             print("Error import ZImagePipeline")
             pass
         self.device: str | None = None
+        self.vae: AutoencoderKL | None = None
+        self.text_encoder: AutoModelForCausalLM | None = None
+        self.tokenizer: AutoTokenizer | None = None
+        self.scheduler: FlowMatchEulerDiscreteScheduler | None
 
     def start(self):
         if torch.cuda.is_available():
             model_path = self.model_path or "Tongyi-MAI/Z-Image-Turbo"
             logger_p.debug("Loading CUDA")
-            self.device = "cuda" 
+            self.device = "cuda"
+            self.load_compo()
             self.pipeline = ZImagePipeline.from_pretrained(
-                model_path,
+                scheduler=None,
+                vae=self.vae,
+                text_encoder=self.text_encoder, 
+                tokenizer=self.tokenizer,
                 torch_dtype=torch.bfloat16,
                 low_cpu_mem_usage=False,
-            ).to(device=self.device)
-
+                transformer=None
+            )
+            
             self.pipeline.to("cuda")
-
+            self.pipeline.vae.disable_tiling()
+            self.load_transformer()
             self.enable_flash_attn()
+            self.load_scheduler()
 
             if(self.compile_dit()):
                 self._warmup()
@@ -290,7 +305,7 @@ class PipelineZImage:
 
     def compile_dit(self):
         try:
-            self.pipeline.transformer.compile()
+            self.pipeline.transformer = torch.compile(self.pipeline.transformer, mode="max-autotune-no-cudagraphs", fullgraph=False)
             logger_p.info("The DiT compilation is complete")
             return True
         except Exception as e:
@@ -301,19 +316,64 @@ class PipelineZImage:
         try:
             logger_p.info("Starting warmup process...")
             warmup_prompt = "a simple test image"
-            _ = self.pipeline(
-                prompt=warmup_prompt,
-                height=512,
-                width=512,
-                num_inference_steps=2,
-                guidance_scale=0.0,
-                generator=torch.Generator(self.device).manual_seed(42),
-            ).images[0]
-        
+            for i in range(3):
+                _ = self.pipeline(
+                    prompt=warmup_prompt,
+                    height=1024,
+                    width=1024,
+                    num_inference_steps=9,
+                    guidance_scale=0.0,
+                    generator=torch.Generator(self.device).manual_seed(42 + i),
+                ).images[0]      
             logger_p.info("Warmup completed successfully")
-        
         except Exception as e:
             logger_p.error(f"X Warmup failed: {str(e)}")
+
+    def load_compo(self):
+        try:
+            self.vae = AutoencoderKL.from_pretrained(
+                self.model_path or "Tongyi-MAI/Z-Image-Turbo",
+                subfolder="vae",
+                torch_dtype=torch.bfloat16,
+                device_map="cuda",
+            )
+
+            self.text_encoder = AutoModelForCausalLM.from_pretrained(
+                self.model_path or "Tongyi-MAI/Z-Image-Turbo",
+                subfolder="text_encoder",
+                torch_dtype=torch.bfloat16,
+                device_map="cuda",
+            ).eval()
+
+            self.tokenizer = AutoTokenizer.from_pretrained(self.model_path or "Tongyi-MAI/Z-Image-Turbo", 
+                    subfolder="tokenizer")
+
+            self.tokenizer.padding_side = "left"
+
+            try:
+                torch._inductor.config.conv_1x1_as_mm = True
+                torch._inductor.config.coordinate_descent_tuning = True
+                torch._inductor.config.epilogue_fusion = False
+                torch._inductor.config.coordinate_descent_check_all_directions = True
+                torch._inductor.config.max_autotune_gemm = True
+                torch._inductor.config.max_autotune_gemm_backends = "TRITON,ATEN"
+                torch._inductor.config.triton.cudagraphs = False
+
+            except Exception as e:
+                logger_p.error(f"X load_compo failed: {str(e)}")
+                pass
+
+        except Exception as e:
+            logger_p.error(f"X load_compo failed: {str(e)}")
+
+    def load_transformer(self):
+        self.transformer = ZImageTransformer2DModel.from_pretrained(
+            self.model_path or "Tongyi-MAI/Z-Image-Turbo", subfolder="transformer").to("cuda", torch.bfloat16)
+        self.pipeline.transformer = self.transformer
+
+    def load_scheduler(self):
+        self.scheduler = FlowMatchEulerDiscreteScheduler(num_train_timesteps=1000, shift=3.0)
+        self.pipeline.scheduler = self.scheduler
 
 
 class ModelPipelineInit:
