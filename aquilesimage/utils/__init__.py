82eb0d503a9840242b0b6d24ce7cbba6bb9f60e5
F4k3r22 <fredyriveraacevedo13@gmail.com>
2025-12-08 17:39:36 -0600

    Test download video models

--- aquilesimage/utils/__init__.py
+++ aquilesimage/utils/__init__.py
@@ -1,2 +1,3 @@
 from .utils import Utils, setup_colored_logger, verify_api_key, create_dev_mode_response, create_dev_mode_video_response
 from .task import VideoTaskGeneration
+from .utils_video import download_base_wan_2_2
--- aquilesimage/utils/utils_video.py
+++ aquilesimage/utils/utils_video.py
@@ -1,5 +1,6 @@
 from platformdirs import user_data_dir
 import os
+from huggingface_hub import hf_hub_download
 
 ## Constant
 
@@ -14,3 +15,7 @@ REPO_ID_WAN_2_2_DISTILL = "lightx2v/Wan2.2-Distill-Models"
 REPO_ID_WAN_2_2_LI = "lightx2v/Wan2.2-Lightning"
 
 BASE_HY_1_5 = "tencent/HunyuanVideo-1.5"
+
+def download_base_wan_2_2():
+    print(f"PATH: {AQUILES_VIDEO_BASE_PATH}/wan_2_2")
+    hf_hub_download(repo_id=BASE_WAN_2_2, filename="wan2.2_ti2v_lightx2v.safetensors", local_dir=f"{AQUILES_VIDEO_BASE_PATH}/wan_2_2")
