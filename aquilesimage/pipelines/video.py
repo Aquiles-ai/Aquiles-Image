84ea5b9a737bd80a5663dfef3f5bfdc335cc845c
F4k3r22 <fredyriveraacevedo13@gmail.com>
2025-12-23 22:36:52 -0600

    Small Fix

--- aquilesimage/pipelines/video.py
+++ aquilesimage/pipelines/video.py
@@ -425,7 +425,7 @@ class Wan2_1_Pipeline:
                 height=480,  
                 width=832,  
                 num_frames=81,
-                guidance_scale=1.0,
+                guidance_scale=2.0,
                 denoising_step_list=[1000, 750, 500, 250]
             )
 
@@ -448,7 +448,7 @@ class Wan2_1_Pipeline:
                 height=480,  
                 width=832,  
                 num_frames=81,
-                guidance_scale=1.0,
+                guidance_scale=2.0,
                 denoising_step_list=[1000, 750, 500, 250]
             )
 
