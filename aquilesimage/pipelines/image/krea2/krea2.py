72a2200b7efdf8168bb22153ec9127cb682bd80a
F4k3r22 <fredyriveraacevedo13@gmail.com>
2026-06-24 18:54:24 -0600

    Fix krea2

--- aquilesimage/pipelines/image/krea2/krea2.py
+++ aquilesimage/pipelines/image/krea2/krea2.py
@@ -30,9 +30,20 @@ class PipelineKrea2(BasePipeline):
         self.conf_lora = conf_lora
 
     def start(self):
-        self.pipeline = Krea2Pipeline.from_pretrained(self.model_name, 
+        import diffusers.utils as _diffusers_utils
+    
+        original_bnb = _diffusers_utils.is_bitsandbytes_available
+        _diffusers_utils.is_bitsandbytes_available = lambda: False
+    
+        try:
+            self.pipeline = Krea2Pipeline.from_pretrained(
+                self.model_name,
                 torch_dtype=torch.bfloat16,
-                quantization_config=None).to("cuda")
+                quantization_config=None
+            ).to("cuda")
+        finally:
+            _diffusers_utils.is_bitsandbytes_available = original_bnb
+        
 
         if self.load_lora:
             loadLoRA(self.pipeline, self.conf_lora)
